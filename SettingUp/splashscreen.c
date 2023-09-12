f9cf839d06849808ed405253e9fe10222c73aac5
Jun Kiat <junkiat.lim@digipen.edu>
2023-09-12 10:58:00 +0800

    Test

--- SettingUp/splashscreen.c
+++ SettingUp/splashscreen.c
@@ -1,6 +1,10 @@
 #include "cprocessing.h"
+#include <math.h>
 
 CP_Image logo;
+int alpha = 255;
+double delta;
+double totalTime = 0;
 
 void splash_screen_init(void)
 {
@@ -13,8 +17,14 @@ void splash_screen_init(void)
 
 void splash_screen_update(void)
 {
+	delta = CP_System_GetDt();
+	totalTime += delta;
+	double time = (int)(totalTime) * 128 % 256;
+	
+	double elapsedTime = fmod(totalTime, 2.0);
+	alpha = (int)(elapsedTime * 255 / 2.0);
 	CP_Graphics_ClearBackground(CP_Color_Create(0, 0, 0, 255));
-	CP_Image_Draw(logo, 0.f, 0.f, CP_Image_GetWidth(logo) / 1.2, CP_Image_GetHeight(logo) / 1.2, 255);
+	CP_Image_Draw(logo, 0.f, 0.f, CP_Image_GetWidth(logo) / 1.2, CP_Image_GetHeight(logo) / 1.2, alpha);
 	if (CP_Input_KeyDown(KEY_Q))
 	{
 		CP_Engine_Terminate();
