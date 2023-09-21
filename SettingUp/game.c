4f7c4e912332cb0152f4de8f0488878f546e371a
Jun Kiat <junkiat.lim@digipen.edu>
2023-09-21 09:57:51 +0800

    updated variables to use player struct

--- SettingUp/game.c
+++ SettingUp/game.c
@@ -6,7 +6,7 @@
 #define NUM_PLAYER 3
 
 CP_Color redColor, greenColor, blueColor, whiteColor;
-CP_Color colors[NUM_PLAYER + 1];
+CP_Color colors[NUM_PLAYER];
 
 float diameter, width, height;
 float triangle_width, triangle_height;
@@ -25,40 +25,29 @@ void Game_Init(void)
 	height = CP_System_GetDisplayHeight() * 0.85f;
 	diameter = 50;
 
+	set_color();
+
 	for (int i = 0; i < NUM_PLAYER; i++) {
 		player[i].rotation = 0;
 		player[i].diameter = diameter;
 		player[i].position = CP_Vector_Set(width * (i + 1) / (NUM_PLAYER + 1), height / 2);
+		player[i].color = colors[i];
 	}
 
 	triangle_width = 20;
 	triangle_height = 25;
 
-	set_color();
-	
 	selected = 0;
 	velocity = 200;
-
-	}
 }
 
 
 void Game_Update(void)
 {
+	movement();
 	CP_Graphics_ClearBackground(CP_Color_Create(100, 100, 100, 255));
 	draw_player();
-	
-	if (CP_Input_MouseTriggered(MOUSE_BUTTON_LEFT)) {
-		check_for_input();
-	}
-
-	if (CP_Input_KeyTriggered(KEY_Q)) {
-		return_to_menu();
-	}
-
-	movement();
-
-	
+	check_for_input();
 }
 
 void Game_Exit(void)
@@ -75,20 +64,26 @@ void draw_player(void) {
 		CP_Settings_Fill(whiteColor);
 		CP_Graphics_DrawTriangleAdvanced(player[i].position.x, player[i].position.y - triangle_height,
 			player[i].position.x - triangle_width, player[i].position.y + triangle_height / 2,
-			player[i].position.x + triangle_width, player[i].position.y + triangle_height / 2, rotation[i]
+			player[i].position.x + triangle_width, player[i].position.y + triangle_height / 2, player[i].rotation
 		);
 	}
 }
 
 
 void check_for_input(void) {
-	for (int i = NUM_PLAYER - 1; i >= 0; i--) {
-		int clicked = IsCircleClicked(player[i].x,
-			player[i].y, diameter, CP_Input_GetMouseX(), CP_Input_GetMouseY());
-		if (clicked == 1) {
-			selected_player = i;
-			selected = 1;
-			break;
+	if (CP_Input_KeyTriggered(KEY_Q)) {
+		return_to_menu();
+	}
+
+	if (CP_Input_MouseTriggered(MOUSE_BUTTON_LEFT)) {
+		for (int i = NUM_PLAYER - 1; i >= 0; i--) {
+			int clicked = IsCircleClicked(player[i].position.x,
+				player[i].position.y, player[i].diameter, CP_Input_GetMouseX(), CP_Input_GetMouseY());
+			if (clicked == 1) {
+				selected_player = i;
+				selected = 1;
+				break;
+			}
 		}
 	}
 }
@@ -99,21 +94,21 @@ void movement(void) {
 		int input_y = CP_Input_KeyDown(KEY_S) - CP_Input_KeyDown(KEY_W);
 
 		if (input_x != 0) {
-			player[selected_player].x += input_x * velocity * CP_System_GetDt();
+			player[selected_player].position.x += input_x * velocity * CP_System_GetDt();
 			if (input_x > 0) {
-				rotation[selected_player] = 90;
+				player[selected_player].rotation = 90;
 			}
 			else {
-				rotation[selected_player] = 270;
+				player[selected_player].rotation = 270;
 			}
 		}
 		else if (input_y != 0) {
-			player[selected_player].y += input_y * velocity * CP_System_GetDt();
+			player[selected_player].position.y += input_y * velocity * CP_System_GetDt();
 			if (input_y > 0) {
-				rotation[selected_player] = 180;
+				player[selected_player].rotation = 180;
 			}
 			else {
-				rotation[selected_player] = 0;
+				player[selected_player].rotation = 0;
 			}
 		}
 	}
@@ -128,5 +123,5 @@ void set_color(void) {
 	colors[0] = redColor = CP_Color_Create(255, 0, 0, 255);
 	colors[1] = greenColor = CP_Color_Create(0, 255, 0, 255);
 	colors[2] = blueColor = CP_Color_Create(0, 0, 255, 255);
-	colors[3] = whiteColor = CP_Color_Create(255, 255, 255, 255);
+	whiteColor = CP_Color_Create(255, 255, 255, 255);
 }
